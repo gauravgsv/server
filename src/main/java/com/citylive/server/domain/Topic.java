b648d986f983a91aff2701da5c7f2b5a31881cd2
Radhe Shyam Lodhi <radheshyamlodhi64@gmail.com>
2020-01-12 00:50:02 +0530

    Topic submit API

---
+++ src/main/java/com/citylive/server/domain/Topic.java
@@ -0,0 +1,33 @@
+package com.citylive.server.domain;
+
+import com.citylive.server.validation.UserExistsValidator;
+import lombok.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.sql.Timestamp;
+
+@Entity
+@Data
+@Builder(toBuilder = true)
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@IdClass(TopicPK.class)
+@Table(name = "Topic")
+public class Topic {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "hibernate_sequence")
+    private Long topicId;
+    @Id
+    @NotNull
+//    @UserExistsValidator
+    private String userName;
+    @NotNull
+    private String question;
+    private Boolean closed;
+    private Double latitude;
+    private Double longitude;
+    private Timestamp time;
+}
